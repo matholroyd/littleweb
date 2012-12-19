32fd53fb91ac0fb07971cc99f81d2d4a838b2a77
Mat Holroyd <matholroyd@gmail.com>
2012-12-19 21:27:06 +1100

    Add some styles to list

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def body_class
+    full_name = controller.class.to_s.underscore.gsub(/[\/_]/, '-').sub(/-controller/, '')
+    [                     full_name,
+         controller.controller_name, 
+                       "#{full_name}-#{controller.action_name}", 
+      "#{controller.controller_name}-#{controller.action_name}"
+      
+    ].uniq.join(' ')
+  end
 end
